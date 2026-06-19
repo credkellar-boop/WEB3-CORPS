397a75d49e01472f48d997ca0775a3b26025530a
Darion Kellar <credkellar@gmail.com>
2026-06-19 04:15:57 -0700

    Add finance module with submodules and exports

---
+++ src/finance/mod.rs
@@ -0,0 +1,7 @@
+pub mod tokenomics;
+pub mod mev_guard;
+pub mod yield_strategist;
+
+pub use tokenomics::TreasuryManager;
+pub use mev_guard::MevGuard;
+pub use yield_strategist::DeFiYieldStrategist;
