0cc39d421bf01c6e656979b72dfe84efb561144a
Darion Kellar <credkellar@gmail.com>
2026-07-01 01:57:52 -0700

    Update compliance_tests.rs

--- tests/compliance_tests.rs
+++ tests/compliance_tests.rs
@@ -1,9 +1,3 @@
-        multi_sig_threshold: 3,
-    });
-
-    manager.register_agent("CFO_UNIT_01".to_string(), cfo);
-    assert!(manager.active_matrix.contains_key("CFO_UNIT_01"));
-
 #[test]
 fn test_legal_clause_verification_parameters() {
     let validator = SmartLegalContractValidator;
